81dcc229ad2f6697b444e45f3340108f82928229
Nikhil_Singh <nikhil_singh@outlook.com>
2021-04-21 20:18:06 +0100

    removed db email and password

--- main/settings.py
+++ main/settings.py
@@ -74,15 +74,6 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'btreb',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'localhost'
-    }
-}
 
 
 # Password validation
