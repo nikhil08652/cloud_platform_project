75c4a760b55f9a36874f4d62c26690feecf60ba3
Nikhil_Singh <nikhil_singh@outlook.com>
2021-04-21 19:53:38 +0100

    added settings for local settings file

--- main/settings.py
+++ main/settings.py
@@ -149,3 +149,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'rahilkumarcool@gmail.com'
 EMAIL_HOST_PASSWORD = 'nikman9768'
 EMAIL_USE_TLS = True
+
+try: 
+    from .local_settings import *
+except ImportError:
+    pass
